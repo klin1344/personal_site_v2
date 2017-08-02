eab4e9fbf1147e8fe555373357d45bbb053f4643
klin1344 <kevlin97@gmail.com>
2017-08-02 10:39:42 -0700

    Use font-awesome for heart icon

--- client/Layout/Landing.js
+++ client/Layout/Landing.js
@@ -3,10 +3,16 @@ import Particles from 'react-particles-js'
 import particlesConfig from './particlesConfig.json'
 const Landing = () => {
   return (
-    <div style={{height: '100%'}}>
-      <Particles className="particles" params={particlesConfig}>   
+    <div style={{height: '100%', verticalAlign:'middle'}}>
+      <Particles style={{position: 'absolute', height: '100%', width: '100%'}} params={particlesConfig}>   
       </Particles>
-      <div className="landing-text" >Hi.  I'm Kevin.</div>
+      <div className="landing-text">{`Hi. I'm Kevin.`}
+        <div className="landing-subtext">
+        I {' '} <i className="fa fa-heart" aria-hidden="true"></i>
+        {'  '} to code.
+      </div>
+    </div>
+      
     </div>
   )
 }
