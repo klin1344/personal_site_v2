bab3a31d8ff4342aa61d9ca945124c84f55413e1
klin1344 <kevlin97@gmail.com>
2017-08-03 13:38:30 -0700

    Make project card tiles more readable

--- client/Layout/About.js
+++ client/Layout/About.js
@@ -18,7 +18,7 @@ const About = () => {
           <div>This summer I was a <span className="emphasized">Software Engineer Intern</span> at <span className="emphasized"><a className="link" href="http://www.qrigroup.com/">QRI International</a></span> in Houston, TX.  I received a return offer and have joined the team as a contracted <span className="emphasized">Software Engineer</span>.</div>
         </div>
         <ButtonsBar></ButtonsBar>
-        <div style={{marginTop: '1em'}}>
+        <div style={{marginTop: '1.5em'}}>
           <div className="panel-header"><i className="fa fa-code" aria-hidden="true"></i> PLACES I'VE WORKED AT</div>
           <WorkPanel></WorkPanel>
         </div>
