881d08d8cbd69791f875244438b53803d2e96f80
klin1344 <kevlin97@gmail.com>
2017-08-29 12:47:53 -0500

    Added internship project website

--- source/client/Layout/About/WorkData.js
+++ source/client/Layout/About/WorkData.js
@@ -7,7 +7,7 @@ const workData = [{
       logo: qriLogo
     },
     back: {
-      list: ['Developed full stack web app, Quantum Toolbox - qDCA, from scratch', 'Built a React/Redux-based frontend for engineers to interactively vet automated oil/gas decline charts', 'Implemented filtering, sorting, and batch grid view functionalities for wells', ' Wrote custom Highcharts plugin to add support for interactive drag-and-drop of decline curves', 'Designed a well data management cloud platform in Node.js and MongoDB to be used with qDCA and shared among all other QRI web apps']
+      list: ['Developed full stack web app, Quantum Toolbox - qDCA, from scratch: qdca.qrigroup.com', 'Built a React/Redux-based frontend for engineers to interactively vet automated oil/gas decline curves', 'Implemented charting, filtering, sorting, and batch grid view functionalities for wells', ' Wrote custom Highcharts plugin to add support for interactive drag-and-drop of decline curves', 'Designed a well data management cloud platform in Node.js and MongoDB to be used with qDCA and shared among all other QRI web apps']
     }
   },
 
