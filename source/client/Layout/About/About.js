26184e412da7d23ce2c01877e47ca9082a3e4452
klin1344 <kevlin97@gmail.com>
2018-05-04 11:38:45 -0700

    Change description and regenerate dist files

--- source/client/Layout/About/About.js
+++ source/client/Layout/About/About.js
@@ -8,17 +8,21 @@ const About = () => {
     <div>
       <img className="profile" src={profile}></img>
 
-      <div style={{textAlign: 'center'}}>
+      <div style={{ textAlign: 'center' }}>
         <p className="header">Hello.</p>
         <div className="about-text">
           <div>My name is <span className="emphasized">Kevin Lin</span>.</div>
-          <div>I am an undergraduate student at <span className="emphasized"><a className="no-decoration" href="http://www.rice.edu/">Rice University</a></span>, studying <span className="emphasized">Computer Science</span>.</div>
-          <div>I participate in <span className="emphasized">hackathons</span> around the country, work on personal projects on <span className="emphasized"><a className="no-decoration" href="https://github.com/klin1344">Github</a></span>, and hack on random stuff.</div>
-          <br/>
-          <div>This summer I was a <span className="emphasized">Software Engineer Intern</span> at <span className="emphasized"><a className="no-decoration" href="http://www.qrigroup.com/">QRI International</a></span> in Houston, TX.  I received a return offer and have joined the team as a contracted <span className="emphasized">Software Engineer</span>.</div>
+          <div>I am an undergraduate student at <span className="emphasized"><a className="no-decoration" href="http://www.rice.edu/">Rice University</a></span> studying <span className="emphasized">Computer Science</span>.</div>
+          <br />
+          <div>I like to build things.</div>
+          <br />
+
+          <div>In the past, I have worked at <span className="emphasized">QRI International, LLC</span>.  This summer, I will be working at <span className="emphasized">Tableau Software </span>.</div>
+          <br />
+
         </div>
         <ButtonsBar></ButtonsBar>
-        <div style={{marginTop: '1.5em'}}>
+        <div style={{ marginTop: '1.5em' }}>
           <div className="panel-header"><i className="fa fa-code" aria-hidden="true"></i> PLACES I'VE WORKED AT</div>
           <WorkPanel></WorkPanel>
         </div>
--- source/client/Layout/About/WorkData.js
+++ source/client/Layout/About/WorkData.js
@@ -13,15 +13,17 @@ const workData = [{
   link: 'https://www.tableau.com'
 }, {
   front: {
-    title: 'August 2017 - present',
+    title: 'August 2017 - April 2018',
     subtitle: 'Software Engineer Contractor',
     location: 'Houston, TX',
     logo: qriLogo,
   },
   back: {
-    list: ['Building REST API to query data from GeoServer data warehouse',
-      'Using Leaflet to build mapping component to visualize GeoServer data for SpeedWise Portfolio Analysis, a web app that automates well analytics'
-    ]
+    list: ['Assisted Corporate Analytics team in development of SpeedWise Portfolio Analysis (SPA), a new web app that automates well analytics',
+      'Built REST API to query and aggregate data from GeoServer and MongoDB',
+      'Used Leaflet in React to build mapping components to visualize data',
+      'Features implemented include spatial data queries, dynamic styling variables, clustering, and two-way marker binding',
+      'Integrated hybrid, Office 365-based authentication layer using Azure AD']
   },
   link: 'http://www.qrigroup.com'
 }, {
@@ -35,7 +37,7 @@ const workData = [{
     list: ['Developed and deployed visualization and analytics platform: qdca.qrigroup.com',
       'Build a React/Redux-based frontend for decline curve analytics with charting, filtering, sorting, and SSR grid view functionalities for improved client performance',
       'Wrote custom Highcharts plugin to support drag-and-drop of decline curves',
-      'Designed a MongoDV schema that minimizes data duplication'
+      'Designed a MongoDB schema that minimizes data duplication'
     ]
   },
   link: 'https://qdca.qrigroup.com'
